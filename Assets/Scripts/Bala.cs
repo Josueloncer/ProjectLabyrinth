ae7fe3376bbda8d338173ea60535a4a545d51a0e
Ed Casillas <eduardo_casillas@hotmail.com>
2018-10-31 22:10:19 -0600

    Ajustes pequeños en los scripts de Bala y Disparar.

--- Assets/Scripts/Bala.cs
+++ Assets/Scripts/Bala.cs
@@ -1,49 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Bala : MonoBehaviour {
 
-    public float speed = 1f;
-    public float tiempoDestruir = 0;
-    public static int numeroBalas;
-    public Transform splash;
+	public float speed = 1f;
+	public float tiempoDestruir = 0;
+	public static int numeroBalas;
+	public Transform splash;
 
-    void OnCollisionEnter(Collision col)
-    {
-        /*
+	void OnCollisionEnter(Collision col) {
+		/*
             ContactPoint[] contact = col.contacts;
             Vector3 startPos = contact[0].point;
             Quaternion startRot = Quaternion.LookRotation(contact[0].normal);
             Instantiate(splash, startPos, startRot);
         */
-            Debug.Log("Collision");
-    }
-
-    // Use this for initialization
-    void Start ()
-    {
-		
+		Debug.Log("Collision");
 	}
-	
+
+	// Use this for initialization
+	void Start() {
+
+	}
+
 	// Update is called once per frame
-	void Update ()
-    {
-        transform.Translate(0, 0, speed);
-        tiempoDestruir += Time.deltaTime;
+	void Update() {
+		transform.Translate(0, 0, speed);
+		tiempoDestruir += Time.deltaTime;
 
-        if (tiempoDestruir >= 2)
-        {
-            numeroBalas -= 1;
-            Destroy(GameObject.FindWithTag("Bala"));
-            if (numeroBalas == 0)
-            {
-                tiempoDestruir = 0;
-            }
-        }
-        
-    }
-
-
-
+		if (tiempoDestruir >= 2) {
+			numeroBalas -= 1;
+			Destroy(GameObject.FindWithTag("Bala"));
+			if (numeroBalas == 0) {
+				tiempoDestruir = 0;
+			}
+		}
+	}
 }
--- Assets/Scripts/Disparar.cs
+++ Assets/Scripts/Disparar.cs
@@ -1,67 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Disparar : MonoBehaviour {
+	public GameObject bala;
+	public float delayTime = 8;
+	public GameObject muzzleflash;
+	private float delayMuzzle = .1f;
+	private float delaySmoke = 1.9f;
+	public AudioClip[] ShootSounds;
+	public AudioSource audioSource;
+	public GameObject smoke;
 
-    public GameObject bala;
-    public float delayTime = 8;
-    public GameObject muzzleflash;
-    private float delayMuzzle = .1f;
-    private float delaySmoke = 1.9f;
-    public AudioSource audioSource;
-    public AudioSource audioSource2;
-    public GameObject smoke;
+	private float contador = 4;
 
-
-    private float contador = 4;
-	// Use this for initialization
-	void Start ()
-    {
-        //audioSource = GetComponent<AudioSource>();
-	}
-	
 	// Update is called once per frame
-	void Update ()
-    {
-		if(Input.GetKey(KeyCode.Mouse0) && contador > delayTime)
-        {
-            Instantiate(bala, transform.position, transform.rotation);
-            Bala.numeroBalas += 1;
-            contador = 0;
-            muzzleflash.SetActive(true);
-            smoke.SetActive(true);
-            int random = Random.Range(1, 3);
-            switch(random)
-            {
-                case 1:
-                    {
-                        audioSource.Play();
-                        break;
-                    }
-                case 2:
-                    {
-                        audioSource2.Play();
-                        break;
-                    }
-            }
-        }
+	void Update() {
+		if (Input.GetKey(KeyCode.Mouse0) && contador > delayTime) {
+			Instantiate(bala, transform.position, transform.rotation);
+			Bala.numeroBalas += 1;
+			contador = 0;
+			muzzleflash.SetActive(true);
+			smoke.SetActive(true);
+			int random = Random.Range(0, ShootSounds.Length);
+			audioSource.PlayOneShot(ShootSounds[random]);
+		}
 
+		if (contador > delayMuzzle) {
+			muzzleflash.SetActive(false);
+		}
 
-        if(contador > delayMuzzle)
-        {
-            muzzleflash.SetActive(false);
-        }
+		if (contador > delaySmoke) {
+			smoke.SetActive(false);
+		}
 
-        if(contador > delaySmoke)
-        {
-            smoke.SetActive(false);
-        }
-
-
-        contador += Time.deltaTime;
-    }
-
-
-
+		contador += Time.deltaTime;
+	}
 }
