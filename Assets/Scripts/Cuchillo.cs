8c1409063ad93d4a374b1d341d92a19fee25740f
Josue <josuelobosgomez@gmail.com>
2018-11-05 00:02:28 -0600

    Proyectos juntos

---
+++ Assets/Scripts/Cuchillo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cuchillo : MonoBehaviour {
+
+    public Animation anima;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            anima.Play("Cortar");
+        }
+
+    }
+}
